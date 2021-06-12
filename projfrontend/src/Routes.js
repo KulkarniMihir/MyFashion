39f30c98e193adba159ea990e0376b7128fb0e7c
Mihir Kulkarni <mikukulkarni@gmail.com>
2021-06-12 10:20:50 +0530

    second commit

--- projfrontend/src/Routes.js
+++ projfrontend/src/Routes.js
@@ -8,6 +8,9 @@ import Signup from './user/Signup';
 
 import UserDashBoard from './user/UserDashBoard';
 import AdminDashBoard from './user/AdminDashBoard';
+import AddCategory from './admin/AddCategory';
+import ManageCategories from './admin/ManageCategories';
+import AddProduct from './admin/AddProduct';
 
 const Routes = () => {
     return (
@@ -18,6 +21,9 @@ const Routes = () => {
                 <Route path="/signin" exact component={Signin} />
                 <PrivateRoute path="/user/dashboard" exact component={UserDashBoard}/>
                 <AdminRoute path="/admin/dashboard" exact component={AdminDashBoard}/>
+                <AdminRoute path="/admin/create/category" exact component={AddCategory}/>
+                <AdminRoute path="/admin/categories" exact component={ManageCategories}/>
+                <AdminRoute path="/admin/create/product" exact component={AddProduct}/>
             </Switch>
         </BrowserRouter>
     );
